d679248c1ea633e1e7a681397339c46aacb1f1ab
katiehan22 <katiehan22@gmail.com>
2022-12-11 23:39:55 -0800

    add instructions screen

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const ctx = canvas.getContext("2d");
 
   const startButton = document.getElementById("start-button");
+  const startButton2 = document.getElementById("start-button2");
   const titleScreen = document.querySelector(".title-screen");
   const canvasDiv = document.querySelector(".canvas-div");
   const gameCanvas = document.getElementById("game-canvas");
+  const instructionsButton = document.getElementById("instructions-button");
+  const instructionsScreen = document.querySelector(".instructions-screen");
 
   const game = new Game(ctx, canvasDiv);
 
@@ -17,5 +20,18 @@ document.addEventListener("DOMContentLoaded", () => {
     gameCanvas.style.display = "block";
 
     game.start();
-  })
+  });
+
+  startButton2.addEventListener("click", function () {
+    instructionsScreen.style.display = "none";
+    canvasDiv.style.display = "block";
+    gameCanvas.style.display = "block";
+
+    game.start();
+  });
+
+  instructionsButton.addEventListener("click", function() {
+    titleScreen.style.display = "none";
+    instructionsScreen.style.display = "block";
+  });
 })
